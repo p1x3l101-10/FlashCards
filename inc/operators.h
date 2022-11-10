f822b3d5515bad5fe6ad4f4cf2d09ba509508e27
p1x3l101-10 <115598784+p1x3l101-10@users.noreply.github.com>
2022-11-10 13:28:50 -0600

    Removing old files in prep for Release 2.0

--- inc/operators.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPERATORS
-#define OPERATORS
-
-void add(int fnum, int frange, int ffloor);
-void subt(int fnum, int frange, int ffloor);
-void multi(int fnum, int frange, int ffloor);
-void divi(int fnum, int frange, int ffloor);
-
-#endif
